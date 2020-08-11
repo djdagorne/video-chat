46f6db2981a726dc1e3bf5f1310b220bd56b5977
Dexter Dagorne <dj.dagorne@gmail.com>
2020-08-11 13:47:27 -0400

    deployin and securin

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
   secure: true,
-  host: 'dexters-vid-app.herokuapp.com/',
+  host: 'peerjs-server.herokuapp.com',
   port: '443',
 });
 const myVideo = document.createElement('video');
